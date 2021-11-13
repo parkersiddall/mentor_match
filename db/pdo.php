346a11db44b15f70bf02aad58351d52145a0ae93
parkersiddall <parker.siddall@unibocconi.it>
2021-11-13 12:55:44 +0100

    building up home page

--- db/pdo.php
+++ db/pdo.php
@@ -1,5 +1,4 @@
 <?php
-    // TODO grant user access to database
     $pdo = new PDO('mysql:host=localhost;port=3306;dbname=mentor_match', 'user', 'password');
     // See the "errors" folder for details...
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
