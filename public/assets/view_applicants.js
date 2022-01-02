487116f3d6b931dcbc36d66c259cc821958bc227
parkersiddall <parker.siddall@unibocconi.it>
2022-01-02 14:39:36 +0100

    finished buttons for match actions

--- public/assets/view_applicants.js
+++ public/assets/view_applicants.js
@@ -149,19 +149,17 @@ $(document).ready(function() {
                         <td>${responseJSON[match]['mentor_last_name']}</td>
                         <td>${responseJSON[match]['mentor_email']}</td>
                         <td>
-                            <div class="btn-group btn-group-sm" role="group">
-                                <button type="button" class="btn btn-outline-secondary match-responses-button" data-bs-toggle="modal" data-bs-target="#applicantResponseModal">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
-                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                        <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-                                    </svg>
-                                </button>
-                                <button type="button" class="btn btn-outline-secondary match-delete-button">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
-                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                                    </svg>
-                                </button>
+                            <div class="dropdown">
+                              <a class="btn btn-outline-secondary btn-sm dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
+                                  <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
+                                </svg>
+                              </a>
+                              <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                <li class="applicant-id" id="match-applicant-id-${responseJSON[match]['mentor_application_id']}"><a class="dropdown-item match-response-button" data-bs-toggle="modal" data-bs-target="#applicantResponseModal">Mentor Response</a></li>
+                                <li class="applicant-id" id="match-applicant-id-${responseJSON[match]['mentee_application_id']}"><a class="dropdown-item match-response-button" data-bs-toggle="modal" data-bs-target="#applicantResponseModal">Mentee Response</a></li>
+                                <li><a class="dropdown-item match-delete-button">Delete</a></li>
+                              </ul>
                             </div>
                         </td>
                     </tr>
@@ -173,6 +171,7 @@ $(document).ready(function() {
 
             // load buttons for each row
             loadMatchDeleteButtons()
+            loadMatchResponseButtons()
 
             // adjust stats
             $('#project-num-matches').text(matchCount)
@@ -186,6 +185,15 @@ $(document).ready(function() {
         $('.applicant-response-button').click(function(event) {
             $("#question-responses-container").empty()
             let application_id = findParent(event, 'applicant-id').id
+            adjustApplicantResponseModal(application_id)
+        })
+    }
+
+    function loadMatchResponseButtons() {
+        $('.match-response-button').click(function(event) {
+            $("#question-responses-container").empty()
+            let application_id = findParent(event, 'applicant-id').id
+            application_id = application_id.replace('match-applicant-id-', '')
             adjustApplicantResponseModal(application_id)
         })
     }
