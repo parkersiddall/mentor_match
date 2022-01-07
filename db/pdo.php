b1a0a7d5c2519053869cca3c079f1ab28efb1090
parkersiddall <parker.siddall@unibocconi.it>
2022-01-07 10:45:40 +0100

    adding composer, moving variables over to env file

--- db/pdo.php
+++ db/pdo.php
@@ -1,11 +1,21 @@
 <?php
+
+    require_once('../vendor/autoload.php');
+
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__."/..");
+    $dotenv->load();
+
+    $connection_string = $_ENV['CONNECTION_STRING'];
+    $user = $_ENV['USER'];
+    $password = $_ENV['PASSWORD'];
+
     try {
-        $pdo = new PDO('mysql:host=localhost;port=3306;dbname=mentor_match', 'user', 'password');
+        $pdo = new PDO($connection_string, $user, $password);
         // See the "errors" folder for details...
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (Exception $e) {
         http_response_code(500);
-        echo json_encode("Internal server error.");
+        echo json_encode('Internal server error.');
         die;
     }
 
