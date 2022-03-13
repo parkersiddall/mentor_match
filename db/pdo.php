757c88ae5a81e87b4e9855d3cf7ad3471f4a75b6
Parker Siddall <parker.siddall@gmail.com>
2022-03-13 15:28:48 +0100

    Minor changes for local development

--- db/pdo.php
+++ db/pdo.php
@@ -6,16 +6,17 @@
     $dotenv->load();
 
     $connection_string = $_ENV['CONNECTION_STRING'];
-    $user = $_ENV['USER'];
-    $password = $_ENV['PASSWORD'];
+    $user = $_ENV['MYSQL_USER'];
+    $password = $_ENV['MYSQL_PASSWORD'];
 
     try {
         $pdo = new PDO($connection_string, $user, $password);
         // See the "errors" folder for details...
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "DB connected!";
     } catch (Exception $e) {
         http_response_code(500);
-        echo json_encode('Internal server error.');
+        echo json_encode($e);
         die;
     }
 
